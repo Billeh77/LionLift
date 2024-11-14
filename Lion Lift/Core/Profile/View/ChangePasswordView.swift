45b967c86be688abaccfb7d2403cbbe61d8cf684
Chase Preston <67482176+cpreston123@users.noreply.github.com>
2024-11-14 17:44:50 -0500

    added structure for profile section

---
+++ Lion Lift/Core/Profile/View/ChangePasswordView.swift
@@ -0,0 +1,18 @@
+//
+//  ChangePasswordView.swift
+//  Lion Lift
+//
+//  Created by Chase Preston on 11/14/24.
+//
+
+import SwiftUI
+
+struct ChangePasswordView: View {
+    var body: some View {
+        Text("Change Password")
+    }
+}
+
+#Preview {
+    ChangePasswordView()
+}
---
+++ Lion Lift/Core/Profile/View/ProfileSettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileSettingsView.swift
+//  Lion Lift
+//
+//  Created by Chase Preston on 11/14/24.
+//
+
+import SwiftUI
+
+struct ProfileSettingsView: View {
+    var body: some View {
+        Text("Profile Settings")
+    }
+}
+
+#Preview {
+    ProfileSettingsView()
+}
