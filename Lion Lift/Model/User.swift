a95696863bfd8c44e7d8bc8223fedd904f2e7637
Billeh77 <billehemile@gmail.com>
2024-12-03 00:59:08 -0500

    Model updated

--- Lion Lift/Model/User.swift
+++ Lion Lift/Model/User.swift
@@ -12,11 +12,15 @@ import MapKit
 struct User: Identifiable, Decodable {
     @DocumentID var id: String?
     let uid: String
-    let username: String
-    let fullname: String
-    let profileImageUrl: String?
-    let phoneNumber: String
-    let email: String
+    var username: String
+    var fullname: String
+    var profileImageUrl: String?
+    var phoneNumber: String
+    var email: String
+    var nextFlightDateAndTime: Timestamp
+    var nextFlightAirport: String
+    var departing: Bool
+    
     var isCurrentUser: Bool { return AuthViewModel.shared.currentUser?.uid == id }
     
     static var dummyUser: User {
@@ -26,7 +30,10 @@ struct User: Identifiable, Decodable {
             fullname: "John Doe",
             profileImageUrl: nil,
             phoneNumber: "0795602606",
-            email: "johndoe@example.com"
+            email: "johndoe@example.com",
+            nextFlightDateAndTime: Timestamp(date: Date()),
+            nextFlightAirport: "JFK",
+            departing: true
         )
     }
 }
