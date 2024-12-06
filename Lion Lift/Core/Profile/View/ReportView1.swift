a232bf074985ac3af50343b19251e7ea40d8aca9
c <jooyeonlee@Chloes-MacBook-Pro-2.local>
2024-12-05 21:56:53 -0500

    Update New CustomerSupportView Outline

---
+++ Lion Lift/Core/Profile/View/ReportView1.swift
@@ -0,0 +1,18 @@
+//
+//  ReportView.swift
+//  Lion Lift
+//
+//  Created by Jooyeon Lee on 12/5/24.
+//
+
+import SwiftUI
+
+struct ReportView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReportView()
+}
