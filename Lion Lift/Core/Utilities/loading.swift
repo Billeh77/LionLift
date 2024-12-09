df2a45a9120374f2efdf46d64adbf2b0e3663bac
Chase Preston <67482176+cpreston123@users.noreply.github.com>
2024-12-09 02:43:13 -0500

    change 'Lion Lyft' to 'Lion Lift" on loading screen

--- Lion Lift/Core/Utilities/loading.swift
+++ Lion Lift/Core/Utilities/loading.swift
@@ -13,17 +13,17 @@ struct LoadingView: View {
         ZStack {
             // Background Color (Full screen)
             Color(red: 0, green: 0.22, blue: 0.40)
-                .edgesIgnoringSafeArea(.all) // Ensures the background covers the entire screen
+                .edgesIgnoringSafeArea(.all)
             
             VStack(spacing: 20) {
                 // Logo
-                Image("lion") // Ensure this image is added to Assets
+                Image("lion")
                     .resizable()
                     .scaledToFit()
-                    .frame(width: 112, height: 112) // Specify logo size
+                    .frame(width: 112, height: 112)
                 
                 // Title
-                Text("Lion Lyft")
+                Text("Lion Lift")
                     .font(.custom("Poppins", size: 50).weight(.bold))
                     .foregroundColor(.white)
             }
